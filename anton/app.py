b7ddcab7ee31dccaea71c4550144a8a5382e20ed
Anton Popov <ademaro@ya.ru>
2022-08-19 17:02:59 +0530

    Переработано формирование заголовков в Views и добавлены уведомления

--- anton/app.py
+++ anton/app.py
@@ -1,19 +1,36 @@
-from extensions import db
-from flask import redirect, render_template, request, url_for
+from flask import flash, g, redirect, render_template, request, url_for
 from flask_security import current_user, login_required, roles_required
+
+from extensions import db
 from forms import DemoForm
 from init import app
 from mail import send_email
-from models import UserSubmit, User, Role, user_datastore
+from models import Role, User, UserSubmit, user_datastore
 
 
 @app.before_first_request
 def init():
     db.create_all()
-    user_datastore.find_or_create_role(name="admin", permissions={"admin-read", "admin-write", "user-read", "user-write", "partner-read", "partner-write"})
-    user_datastore.find_or_create_role(name="monitor", permissions={"admin-read", "user-read"})
-    user_datastore.find_or_create_role(name="partner", permissions={"partner-read", "partner-write"})
-    user_datastore.find_or_create_role(name="user", permissions={"user-read", "user-write"})
+    user_datastore.find_or_create_role(
+        name="admin",
+        permissions={
+            "admin-read",
+            "admin-write",
+            "user-read",
+            "user-write",
+            "partner-read",
+            "partner-write",
+        },
+    )
+    user_datastore.find_or_create_role(
+        name="monitor", permissions={"admin-read", "user-read"}
+    )
+    user_datastore.find_or_create_role(
+        name="partner", permissions={"partner-read", "partner-write"}
+    )
+    user_datastore.find_or_create_role(
+        name="user", permissions={"user-read", "user-write"}
+    )
     user_datastore.find_or_create_role(name="reader", permissions={"user-read"})
     db.session.commit()
 
@@ -21,41 +38,51 @@ def init():
 @app.route("/", methods=["GET", "POST"])
 def index():
     """Показ главной страницы."""
-    page_title = "Главная"
-
-    return render_template("index.j2", page_title=page_title, is_index=True)
+    g.page_title = "Главная"
+    return render_template("index.j2", is_index=True)
 
 
 @app.get("/admin")
 @login_required
-@roles_required('admin')
+@roles_required("admin")
 def admin():
+    """Главная страница админки."""
+    g.page_title = "Админка"
     return render_template("admin/index.j2")
 
 
 @app.get("/admin/users")
-@roles_required('admin')
+@roles_required("admin")
 def admin_users():
+    """Список всех пользователей сайта."""
     user_list_db = User.query.all()
+    g.page_title = "Список пользователей"
     return render_template("admin/users.j2", users=user_list_db)
 
 
 @app.get("/admin/user/<int:user_id>/roles")
-@roles_required('admin')
+@roles_required("admin")
 def admin_user_roles(user_id):
+    """Управление ролями конкретного пользователя."""
     user_db = User.query.get_or_404(user_id)
     roles_db = Role.query.all()
+    g.page_title = f"Управление ролями для пользователя {user_db.email}"
     return render_template("admin/roles.j2", user=user_db, roles=roles_db)
 
 
 @app.get("/admin/user/<int:user_id>/<int:role_id>/add")
-@roles_required('admin')
+@roles_required("admin")
 def admin_user_role_add(user_id, role_id):
+    """Добавление роли пользователю."""
     user_db = User.query.get_or_404(user_id)
     role_db = Role.query.get_or_404(role_id)
     user_datastore.add_role_to_user(user_db, role_db)
     db.session.commit()
-    return redirect(url_for('admin_user_roles', user_id=user_id))
+    flash(
+        f"Успешно добавлена роль «{role_db.name}» пользователю «{user_db.email}»",
+        "success",
+    )
+    return redirect(url_for("admin_user_roles", user_id=user_id))
 
 
 @app.get("/admin/user/<int:user_id>/<int:role_id>/remove")
@@ -78,46 +105,45 @@ def admin_user_role_remove(user_id, role_id):
 
 @app.route("/mail", methods=["GET", "POST"])
 def test_mail():
-    page_title = "Главная"
+    """Тестовая отправка письма админам."""
     send_email("Тестовое письмо")
-    return render_template("index.j2", page_title=page_title)
+    g.page_title = "Тестовая отправка письма"
+    return render_template("index.j2")
 
 
 @app.get("/lk")
 @login_required
 def lk():
     """Личный кабинет."""
-    page_title = "Личный кабинет"
-
-    return render_template('lk/index.j2')
+    g.page_title = "Личный кабинет"
+    return render_template("lk/index.j2")
 
 
 @app.route("/test")
 def test():
     """Показ тестовой страницы."""
-    page_title = "Тестовая страница"
-    return render_template("test.j2", page_title=page_title)
+    g.page_title = "Тестовая страница"
+    return render_template("test.j2")
 
 
 @app.route("/users")
 def users():
     """Вывод списка пользователей."""
-    page_title = "Список пользователей, кто заполнил форму"
     user_list_db = UserSubmit.query.all()
-    return render_template("users.j2", page_title=page_title, users=user_list_db)
+    g.page_title = f"Список пользователей, кто заполнил форму: {len(user_list_db)}"
+    return render_template("users.j2", users=user_list_db)
 
 
 @app.route("/thanks")
 def thanks():
     """При успешной отправке формы."""
-    page_title = "Спасибо за заполнение формы!"
-    return render_template("thanks.j2", page_title=page_title)
+    g.page_title = "Спасибо за заполнение формы!"
+    return render_template("thanks.j2")
 
 
 @app.route("/form", methods=["GET", "POST"])
 def demo_form():
     """Форма для отправки и сохранение в БД."""
-    page_title = "Демо-форма"
     form = DemoForm()
     if form.validate_on_submit():
         print(
@@ -130,8 +156,7 @@ def demo_form():
         db.session.add(user_db)
         db.session.commit()
 
-        user_list_db = UserSubmit.query.all()
-        for user in user_list_db:
-            print(user.id, user.name, user.email)
+        flash("Форма успешно отправлена добавлена", "success")
         return redirect("thanks")
-    return render_template("test-form.j2", page_title=page_title, form=form)
+    g.page_title = "Демо-форма"
+    return render_template("test-form.j2", form=form)
